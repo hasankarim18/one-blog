05b881b24c6881d3d2a54dc00b28fd3eae83b40e
hasankarim18 <hallysust@gmail.com>
2023-01-11 15:12:47 +0600

    topbar menubar separated and blank page created with card

---
+++ admin/blank.php
@@ -0,0 +1,39 @@
+<?php include './inc/header.php' ?>
+
+
+
+<!-- ########## START: MAIN PANEL ########## -->
+
+
+
+<div class="br-pagetitle">
+    <i class="icon ion-ios-home-outline"></i>
+    <div>
+        <h4>Blank page template</h4>
+        <p class="mg-b-0">Blank page template</p>
+    </div>
+</div>
+
+<div class="br-pagebody">
+
+    <div class="row row-sm">
+        <div class="col-lg-12">
+            <div class="card bd-0 shadow-base p-4">
+                <h6 class="tx-13 tx-uppercase tx-inverse tx-semibold tx-spacing-1">
+                    How Engaged Our Users Daily
+                </h6>
+                <!-- page content start  -->
+
+                <!-- page content end -->
+            </div><!-- d-flex -->
+        </div>
+    </div>
+</div>
+
+</div>
+
+<!-- br-pagebody -->
+
+
+
+<?php include './inc/footer.php'; ?>
